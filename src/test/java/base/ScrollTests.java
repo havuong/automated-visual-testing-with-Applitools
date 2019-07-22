5537c974bcee1c2aafe8062105b7254e78002f26
havuong <havuong138@gmail.com>
2019-07-22 13:49:40 +0700

    [setForceFullPageScreenshot] for Large & Deep DOM

---
+++ src/test/java/base/ScrollTests.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.junit.Test;
+
+public class ScrollTests extends BaseTests {
+
+    @Test
+    public void testLargeDom() {
+        driver.get(System.getProperty("site.largedom.url"));
+        eyesManager.getEyes().setForceFullPageScreenshot(true);
+        eyesManager.validateWindow();
+    }
+}
